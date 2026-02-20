fcea1aa1057095620ef931767b9dbc4a65a5218a
sohaibturk <sohaibajmalturk@gmail.com>
2026-02-20 04:20:05 -0800

    add product controller and dto

---
+++ Producer/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Producer.Dtos;
+using Producer.RabbitMQ;
+
+namespace Producer.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly IMessageProducer _messageProducer;
+
+    public ProductController(IMessageProducer messageProducer)
+    {
+        _messageProducer = messageProducer;
+    }
+
+    [HttpPost("products")]
+    public IActionResult Create([FromBody] ProductDto product)
+    {
+        _messageProducer.Send(product);
+
+        return Ok(new
+        {
+            Message = "Product created and sent to RabbitMQ",
+            Product = product
+        });
+    }
+}
---
+++ Producer/Dtos/ProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Producer.Dtos;
+
+public class ProductDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Description { get; set; }
+    public string Sku { get; set; }
+}
